8e861294dbc76adfb2eec2c452e7f35b0c452b71
かる <karuhi.dev@gmail.com>
2023-01-25 23:41:31 +0900

    feat: eslintのアプデに対応

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    rules: {
+      semi: 'error',
+      'prefer-const': 'error',
+    },
+
+    root: true,
+    env: {
+      browser: true,
+      node: true,
+    },
+    parserOptions: {
+      parser: 'babel-eslint',
+    },
+    extends: [
+      '@nuxtjs',
+      'prettier',
+      'prettier/vue',
+      'plugin:prettier/recommended',
+      'plugin:nuxt/recommended',
+    ],
+    plugins: ['prettier'],
+    // add your custom rules here
+    rules: {
+      'vue/no-v-html': 'off',
+    },
+  },
+];
